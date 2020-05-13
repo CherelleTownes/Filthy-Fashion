5c4de791f0c50bed62dd8ec31955b74c861ca922
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-05-13 12:53:50 -0400

    css reset and clean slate react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import GlobalStyle from './components/shared/GlobalStyle';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <h1>Hello World</h1>
       </div>
     )
