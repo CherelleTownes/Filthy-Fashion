a5beee76b5bb6b6cb8811bf212a70dbe10fa70aa
Cherelle Townes <cherelletownes@Cherelles-MacBook-Air.local>
2020-05-15 16:52:50 -0400

    completed clothing variation css

--- client/src/components/ClothingVariations/index.js
+++ client/src/components/ClothingVariations/index.js
@@ -1,5 +1,90 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+
+const StyledClothingVariations = styled.section`
+  text-align: center;
+
+`;
+
+const Title = styled.h1`
+    font-size: 32px;
+    font-weight: 300;
+    margin-top: 50px;
+    padding-bottom: 40px;
+    color: #8C1B1B;
+`;
+
+const FormLabel = styled.h3`
+  font-size: 28;
+  font-weight: 400;
+  color: #8C1B1B;
+  padding-bottom: 20px;
+  padding-top: 30px;
+`;
+
+const Description = styled.input`
+  width: 85%;
+  height: 150px;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  /* resize: none; */
+  font-size: 16px;
+  font-weight: 300;
+`;
+
+const Color = styled.input`
+  width: 25%;
+  padding: 12px 20px;
+  font-size: 16px;
+  color:  #8C1B1B;
+  font-weight: 300;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #f8f8f8;
+  box-sizing: border-box;
+  margin-bottom:20px;
+`;
+
+const StyledLink = styled(Link)`
+font-size: 24px;
+font-weight: 300;
+margin-top: 20px;
+padding-bottom: 10px;
+text-decoration: underline;
+color: #D9304F;
+:hover {
+  background-color: #BF6F6F;
+  color: white;}
+`;
+
+const Button = styled.button`
+  width: 25%;
+  background-color: #F2C1AE; 
+  color: #8C1B1B; 
+  border: 2px solid #BF6F6F;
+  padding: 26px 42px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+  :hover {
+  background-color: #BF6F6F;
+  color: white;
+  }
+`;
+
+
 
 export default class ClothingVariations extends Component {
   state = {
@@ -17,14 +102,15 @@ export default class ClothingVariations extends Component {
   render() {
     const { description, color } = this.state;
     return (
+      <StyledClothingVariations>
       <form onSubmit={(e) => {
         e.preventDefault();
         this.props.handleClothingVariationSubmit(this.state);
         this.props.history.push('/clothing_variations');
       }}>
-        <h3>Design Your Item</h3>
-        <label htmlFor="description">Description</label>
-        <input
+        <Title>Design Your Item</Title>
+        <FormLabel htmlFor="description">Description</FormLabel>
+        <Description
           id="description"
           type="text"
           name="description"
@@ -32,19 +118,21 @@ export default class ClothingVariations extends Component {
           onChange={this.handleChange}
         />
         <br />
-        <label htmlFor="color">Color</label>
-        <input
+        <FormLabel htmlFor="color">Color</FormLabel>
+        <Color
           id="color"
           type="text"
           name="color"
           value={color}
           onChange={this.handleChange}
         />
-        <br />
-        <Link to='/categories'>Pick a New Item</Link>
-        <button>Submit</button>
+          <br />
+          <div>
+          <StyledLink to='/categories'>Pick a New Item</StyledLink>
+          </div>
+        <Button>Submit</Button>
       </form>
-      
+      </StyledClothingVariations>
     )
   }
 }
